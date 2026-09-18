d1f6e86b6dd3565f335d31a30c88fb59ec8f34a8
2025b0101149 <2025b0101149@users.noreply.github.com>
2026-09-18 22:56:52 +0530

    reverse a no using recursion

---
+++ reverse-a-no.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n, int rev = 0) {
+
+    if (n == 0)
+        return rev;
+
+    rev = rev * 10 + n % 10;
+
+
+    return reverseNumber(n / 10, rev);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "Reverse = " << reverseNumber(n);
+
+    return 0;
+}
